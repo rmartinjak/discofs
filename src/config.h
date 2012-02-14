9ca1836703cad3ab15b3149baf1d16766cd27500
Robin Martinjak <rob@rmartinjak.de>
2012-02-14 18:51:01 +0100

    ignore src/config.h (generated by configure)

--- src/config.h
+++ src/config.h
@@ -1,11 +1,6 @@
-/* fs2go - takeaway filesystem
- * Copyright (c) 2012 Robin Martinjak
- * see LICENSE for full license (BSD 2-Clause)
- */
-
 #define PROG_NAME "fs2go"
 #define PROG_VERSION "0"
-#define HG_REV "46+:32c081724bd3+"
+#define HG_REV "49+:3953243d89c6+"
 #define HAVE_ATTR_XATTR_H 1
 #define HAVE_UTIMENSAT 1
 #define HAVE_CLOCK_GETTIME 1
