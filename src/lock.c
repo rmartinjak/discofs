4956dae893aac5a0f4f9c4b0c2e896b663b2a6a2
Robin Martinjak <rob@rmartinjak.de>
2012-05-08 22:57:30 +0200

    use new bst (without duplicate keys)

--- src/lock.c
+++ src/lock.c
@@ -50,7 +50,25 @@ int set_lock(const char *path, int type)
     pthread_mutex_lock(&m_lock_tree);
 
     if (type == LOCK_OPEN)
-        res = bst_insert_dup(lock_tree, hash, NULL);
+    {
+        size_t *c;
+        c = bst_get(lock_tree, hash);
+        if (!c)
+        {
+            if ((c = malloc(sizeof *c)))
+            {
+                *c = 0;
+                res = bst_insert(lock_tree, hash, c);
+            }
+            else
+                res = -1;
+        }
+        else
+        {
+            *c += 1;
+            res = 0;
+        }
+    }
     else
         res = bst_insert(lock_tree, hash, NULL);
 
@@ -71,7 +89,23 @@ int remove_lock(const char *path, int type)
     hash |= type;
 
     pthread_mutex_lock(&m_lock_tree);
-    res = bst_remove(lock_tree, hash, NULL);
+
+    if (type == LOCK_OPEN)
+    {
+        size_t *c;
+        c = bst_get(lock_tree, hash);
+        if (c)
+        {
+            res = 0;
+            if (--(*c) == 0)
+                res = bst_remove(lock_tree, hash, free);
+        }
+        else
+            res = -1;
+    }
+    else
+        res = bst_remove(lock_tree, hash, NULL);
+
     pthread_mutex_unlock(&m_lock_tree);
 
     return res;
