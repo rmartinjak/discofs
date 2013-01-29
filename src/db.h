f5aee61514c18e2996455ed84d46ee0dca26cecb
Robin Martinjak <rob@rmartinjak.de>
2013-01-29 16:50:43 +0100

    comment db.h

--- src/db.h
+++ src/db.h
@@ -55,10 +55,14 @@
 void db_open(void);
 void db_close(void);
 
+
+/* initialize sqlite db in file _fn_.
+   if _clear_ is non-zero, remove ALL the data */
 int db_init(const char *fn, int clear);
 int db_destroy(void);
 
 
+/* get/set "config values" */
 int db_cfg_delete(const char *option);
 int db_cfg_set_int(const char *option, int val);
 int db_cfg_set_str(char *option, const char *val);
@@ -66,32 +70,67 @@ int db_cfg_get_int(const char *option, int *buf);
 int db_cfg_get_str(const char *option, char **buf);
 
 
+/********
+ * JOBS *
+ ********/
+
+/* store a job in the database */
 int db_job_store(const struct job *j);
+
+/* get the next highest-priority job */
 int db_job_get(struct job **j);
+
+/* return non-zero if a job matching _path_ and _opmask_ exits in the db */
 int db_job_exists(const char *path, int opmask);
 
+/* delete job with _id_ */
 int db_job_delete_id(job_id id);
-int db_job_delete(const char* path, int opmask);
+
+/* delete all jobs for path _path_ with a job mathching _opmask_ */
+int db_job_delete(const char *path, int opmask);
+
+/* delete all RENAME jobs, where the new name equals _path_ */
 int db_job_delete_rename_to(const char *path);
 
-
-int db_load_sync(sync_load_cb_t callback);
-int db_store_sync(const struct sync *s);
-
-int db_hardlink_get(ino_t inode, queue *q);
-int db_hardlink_add(const char *path, ino_t inode);
-int db_hardlink_remove(const char *path);
-
-
 int db_job_delete_path(const char *path);
+
+/* rename job entries */
 int db_job_rename_file(const char *from, const char *to);
 int db_job_rename_dir(const char *from, const char *to);
 
+
+/********
+ * SYNC *
+ ********/
+
+/* load all sync entries from the db and pass them to _callback_ */
+int db_load_sync(sync_load_cb_t callback);
+
+/* store one sync entry */
+int db_store_sync(const struct sync *s);
+
+/* delete sync entry maching _path_ */
 int db_sync_delete_path(const char *path);
+
+/* rename sync entries */
 int db_sync_rename_file(const char *from, const char *to);
 int db_sync_rename_dir(const char *from, const char *to);
 
+
+/*************
+ * HARDLINKS *
+ *************/
+
+/* stores all hardlinks that poit to _inode_ in _q_ */
+int db_hardlink_get(ino_t inode, queue *q);
+
+/* add / remove hardlinks */
+int db_hardlink_add(const char *path, ino_t inode);
+int db_hardlink_remove(const char *path);
+
 int db_hardlink_delete_path(const char *path);
+
+/* rename hardlink entries */
 int db_hardlink_rename_file(const char *from, const char *to);
 int db_hardlink_rename_dir(const char *from, const char *to);
 
